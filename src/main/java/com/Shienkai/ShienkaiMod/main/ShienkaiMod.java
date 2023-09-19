926f2e3026a49511d41ef977d973d57ecac9ea54
kamanta0624 <shintaro.kamata@gmail.com>
2023-09-19 14:37:57 +0900

    first commit

---
+++ src/main/java/com/Shienkai/ShienkaiMod/main/ShienkaiMod.java
@@ -0,0 +1,12 @@
+package com.Shienkai.ShienkaiMod.main;
+
+import net.minecraftforge.fml.common.Mod;
+
+@Mod("shienkaimod")
+public class ShienkaiMod {
+    public static final String MOD_ID = "shienkaimod";
+
+    public ShienkaiMod(){
+
+    }
+}
